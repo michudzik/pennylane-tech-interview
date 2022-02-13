918df92da6eeb416c581242f1a2d9e6970bcc06f
Michał Chudzik <m.chudzik@binarapps.com>
2022-02-13 23:19:07 +0100

    Route frontend app

--- app/javascript/entrypoint.tsx
+++ app/javascript/entrypoint.tsx
@@ -1,12 +1,16 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Recipes from "./components/Recipes";
-
-const App = () => {
-  return <Recipes />;
-};
 
 document.addEventListener("DOMContentLoaded", () => {
   const rootEl = document.getElementById("app");
-  ReactDOM.render(<App />, rootEl);
+  ReactDOM.render(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Recipes />} />
+      </Routes>
+    </BrowserRouter>,
+    rootEl
+  );
 });
