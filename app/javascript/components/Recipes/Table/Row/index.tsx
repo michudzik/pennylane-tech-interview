5fe67992a9f90b6c105f9cfbc9c834e1e9063329
Michał Chudzik <m.chudzik@binarapps.com>
2022-02-15 13:06:15 +0100

    Apply simple styling to the Table
    The technical task mentioned to not care about styling and UI in
    general, but the table is horrbile to look at with different alignments
    and the amount of data, which it provides.
    
    This commit aligns table headers with the actual table contents.

--- app/javascript/components/Recipes/Table/Row/index.tsx
+++ app/javascript/components/Recipes/Table/Row/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Recipe from "../../types/recipe";
 import Ingredient from "../../types/ingredient";
 import Tag from "../../types/tag";
+import "../index.css";
 
 const Row = ({ recipe }: { recipe: Recipe }) => {
   return (
@@ -11,22 +12,24 @@ const Row = ({ recipe }: { recipe: Recipe }) => {
           <img src={recipe.image.url} height="50px" width="50px" />
         )}
       </td>
-      <td>{recipe.name}</td>
-      <td>{recipe.author.username}</td>
-      <td>{recipe.tip}</td>
-      <td>{recipe.difficulty}</td>
-      <td>{recipe.budget}</td>
-      <td>{recipe.rate}</td>
-      <td>{recipe.quantity_of_people}</td>
-      <td>{recipe.prep_time}</td>
-      <td>{recipe.cook_time}</td>
-      <td>{recipe.total_time}</td>
-      <td>
+      <td class="align-left">{recipe.name}</td>
+      <td class="align-left">{recipe.author.username}</td>
+      <td class="align-left">{recipe.tip}</td>
+      <td class="align-left">{recipe.difficulty}</td>
+      <td class="align-left">{recipe.budget}</td>
+      <td class="align-right">{recipe.rate}</td>
+      <td class="align-right">{recipe.quantity_of_people}</td>
+      <td class="align-right">{recipe.prep_time}</td>
+      <td class="align-right">{recipe.cook_time}</td>
+      <td class="align-right">{recipe.total_time}</td>
+      <td class="align-left">
         {recipe.ingredients
           .map((ingredient: Ingredient) => ingredient.name)
           .join(", ")}
       </td>
-      <td>{recipe.tags.map((tag: Tag) => tag.name).join(", ")}</td>
+      <td class="align-left">
+        {recipe.tags.map((tag: Tag) => tag.name).join(", ")}
+      </td>
     </tr>
   );
 };
